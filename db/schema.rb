5b23cd9b844636bd790ea0f681ecf2a07d6b7a3c
Dibyan Maharjan <dibyan@acd.edu.np>
2017-09-18 13:45:51 +0000

    Setting Up Routes

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170918130729) do
+ActiveRecord::Schema.define(version: 20170918132302) do
+
+  create_table "posts", force: :cascade do |t|
+    t.integer  "user_id"
+    t.string   "photo"
+    t.text     "description"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+  end
+
+  add_index "posts", ["user_id"], name: "index_posts_on_user_id"
 
   create_table "u_sers", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
