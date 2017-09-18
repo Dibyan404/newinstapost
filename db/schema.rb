7e00a7b63a3105f40a90594167398ef7261ca47d
Dibyan Maharjan <dibyan@acd.edu.np>
2017-09-18 13:11:54 +0000

    Installing CarrierWave

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170918125725) do
+ActiveRecord::Schema.define(version: 20170918130729) do
 
   create_table "u_sers", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
@@ -45,6 +45,7 @@ ActiveRecord::Schema.define(version: 20170918125725) do
     t.string   "name"
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
+    t.string   "photo"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
