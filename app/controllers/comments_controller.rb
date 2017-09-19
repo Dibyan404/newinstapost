258adeea8bd71564c822241c27bbcb09aa8e7be6
Dibyan Maharjan <dibyan@acd.edu.np>
2017-09-19 17:31:02 +0000

    displaying user information

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -20,6 +20,7 @@ class CommentsController < ApplicationController
         @comment = Comment.find(params[:id])
     end
     
+    
     def destroy
         @comment = Comment.find(params[:id])
         @comment.destroy
