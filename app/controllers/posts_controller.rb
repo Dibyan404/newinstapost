a23ad404e6f20c1d22d5b67517d8035affeb183d
Dibyan Maharjan <dibyan@acd.edu.np>
2017-09-19 05:51:09 +0000

    Updating Posts

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,3 +1,12 @@
+#1. User Opens his browser, types in a URL and presses Enter. When a user presses Enter, the browser makes a request for that URL.
+#2. The request hit Rails Route{Config/routes.rb}
+#3. The router maps the URL to the correct controller and action to handle the request.
+#4. The controller receives the request and asks the model to fetch data from the database.
+#5. The model returns a list of data to the controller action.
+#6. The controller action passes the data on to the view.
+#7. The view renders the page as HTML
+#8. The controller sends the HTML back to the browser. The page reloads and the user sees it.
+
 class PostsController < ApplicationController
      before_action :authenticate_user!, only: [:new, :create]
     #def create 
@@ -25,7 +34,24 @@ class PostsController < ApplicationController
     def index
         @posts = Post.all.order('created_at DESC')
         #Post.all will give us all of the posts in the database.
+        #change the order of our collection of Posts by attaching the order method and giving it the parameter 'created_at DESC'. .order('created_at DESC') is saying, order it by the created_at attribute in descending order.
     end
+    
+    def edit
+        #when the edit method in ideas_controller.rb is triggered, Rails will try to find a file called edit.html.erb inside app/views/ideas.
+        @post = Post.find(params[:id])
+        #What Post.find(params[:id]) is doing is, it's finding a Post with an id that matches the params[:id].
+    end
+    def update
+        @post = Post.find(params[:id])
+        @post.update(post_params)
+        if @post.valid?
+            redirect_to root_path
+        else
+            render :edit, status: :unprocessable_entity
+        end
+    end
+    
     
     private
     
@@ -33,4 +59,5 @@ class PostsController < ApplicationController
         #the purpose of post_params is to make sure bad inputs aren't saved in the database. 
         params.require(:post).permit(:user_id, :photo, :description)
     end
+    
 end
