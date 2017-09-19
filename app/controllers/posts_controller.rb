9c5d47c06185d1dbbce928e02379863b14bddddb
Dibyan Maharjan <dibyan@acd.edu.np>
2017-09-19 05:11:15 +0000

    Displaying the Posts

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,10 +1,11 @@
 class PostsController < ApplicationController
+     before_action :authenticate_user!, only: [:new, :create]
     #def create 
         #@post = Post.find(params[:id])
         #@post.create(post_params)
         #redirect_to root_path
     #end
-    before_action :authenticate_user!, only: [:new, :create]
+    
     
     def create
         @post = current_user.posts.create(post_params)
@@ -17,14 +18,19 @@ class PostsController < ApplicationController
         end
     end
     
+    def new
+        @post = Post.new
+    end
+    
+    def index
+        @posts = Post.all.order('created_at DESC')
+        #Post.all will give us all of the posts in the database.
+    end
+    
     private
     
     def post_params
         #the purpose of post_params is to make sure bad inputs aren't saved in the database. 
         params.require(:post).permit(:user_id, :photo, :description)
-    end
-    
-    def new
-        @post = Post.new
     end
 end
