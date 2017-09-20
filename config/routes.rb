dd4ea045ec6a247b0aa7fa730e32210d2aaf9de5
Dibyan Maharjan <dibyan@acd.edu.np>
2017-09-20 19:17:08 +0000

    Styling Everything

--- config/routes.rb
+++ config/routes.rb
@@ -10,7 +10,7 @@ Rails.application.routes.draw do
   resources :post do
     resources :comments
   end
-  resources :users, only: :show
+  resources :users
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 
